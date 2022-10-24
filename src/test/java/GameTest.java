59f3be23fd4a972c0ee177def918fd46a7421476
ikaslea <ikaslea@ikaslea-SATELLITE-L735>
2022-10-24 23:41:45 +0200

    lehen mockito sccannerrra funtzionatzen

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GameTest {
+	
+	@InjectMocks
+	private Game game;
+	
+	@Mock
+	Scanner scanner;
+	
+	@Test
+	public void gameTest() {
+		Mockito.when(scanner.nextLine()).thenReturn("Quit");
+//		Game game = new Game();
+		game.play();
+		
+	}
+
+}
