6231b20da30a3fb24ed4222fd9298813ff33fd95
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2023-10-22 17:46:38 +0200

    first commit

---
+++ src/main/java/secuencia/EjercicioProba.java
@@ -0,0 +1,18 @@
+package secuencia;
+
+import java.util.Scanner;
+
+public class EjercicioProba {
+
+	Scanner sc1 = new Scanner(System.in);
+	
+	public void run() {
+		System.out.println("bi zenbaki");
+		int n1 = Integer.parseInt(sc1.nextLine());
+		System.out.println("bi zenbaki");
+		int n2 = Integer.parseInt(sc1.nextLine());
+		
+		System.out.print(n1 + n2);
+	}
+
+}
---
+++ src/main/java/secuencia/EjercicioProbaMain.java
@@ -0,0 +1,11 @@
+package secuencia;
+
+public class EjercicioProbaMain {
+
+	public static void main(String[] args) {
+		EjercicioProba ejercicioProba = new EjercicioProba();
+		ejercicioProba.run();
+		
+	}
+
+}
