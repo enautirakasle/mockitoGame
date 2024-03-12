77ffc56575ef55588057f4680bb07ec8a9992852
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2024-03-12 22:22:03 +0100

    bug batzuk

--- src/test/java/adibidea01/ApplicationTest.java
+++ src/test/java/adibidea01/ApplicationTest.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import static org.mockito.Mockito.when;
 
